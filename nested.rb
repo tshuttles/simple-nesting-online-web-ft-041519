c13d642b567559ab7660a279ec5daa1837a64e10
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-18 03:43:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -36,6 +36,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+  programmer_hash[:alan_kay]
 end
 
 def dennis_ritchies_language
@@ -54,6 +55,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+    
 end
 
 def adding_matz
