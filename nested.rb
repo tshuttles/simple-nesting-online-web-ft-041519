7ec18f92b3af9ed67ed5f07f06c8d719788c9e5a
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-18 03:46:04 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -36,7 +36,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-  return programmer_hash["Object Orientation"]
+  programmer_hash[:alan_kay]["Object Orientation"]
 end
 
 def dennis_ritchies_language
