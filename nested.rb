3546ff6c8f1050e5e600b1833090a5b4d2646bff
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-18 04:07:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -91,7 +91,7 @@ def changing_alan
           :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
-          :known_for => "Object Orientation", "GUI"
+          :known_for => "Object Orientation", "GUI",
           :languages => ["Smalltalk", "LISP"]
         },
         :dennis_ritchie => {
